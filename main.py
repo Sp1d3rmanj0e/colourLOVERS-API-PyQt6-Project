5ce7f396cdad9a30806190c7281886d6c7499d28
Pelkey_Jason <pelkj662@hsd.k12.or.us>
2024-05-24 12:33:51 -0700

    renamed all variables and added new sprites

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from design import Ui_MainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
-class Example(QWidget):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         
-        self.initUI()
-        
-    def initUI(self):
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Hide Widget Example')
-        
-        self.btn_hide = QPushButton('Hide Widget', self)
-        self.btn_hide.clicked.connect(self.hideWidget)
-        self.btn_hide.setGeometry(10, 10, 100, 30)
-        
-        self.widget_to_hide = QPushButton('Widget to Hide', self)
-        self.widget_to_hide.setGeometry(10, 50, 100, 100)
-        
-        self.show()
-        
-    def hideWidget(self):
-        self.widget_to_hide.hide()  # Hide the widget
 
 def main():
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec())
+    app = QApplication([])
+    window = Window()
+
+    window.show()
+    app.exec()
 
 if __name__ == '__main__':
     main()
