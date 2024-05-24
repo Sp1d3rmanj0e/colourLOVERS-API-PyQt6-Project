492185381953dab36d8bf50e1c6331815ca77d90
Pelkey_Jason <pelkj662@hsd.k12.or.us>
2024-05-24 15:30:29 -0700

    Added color randomizer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# from colourlovers.clapi import ColourLovers
+# cl = ColourLovers()
+# print(cl.search_colors(True, request='random', format = 'json'))
+
+import random
+
 from design import Ui_MainWindow
 from PyQt6.QtWidgets import QApplication, QMainWindow, QColorDialog
-import requests
 
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -23,19 +28,30 @@ class Window(QMainWindow, Ui_MainWindow):
             color = dialog.currentColor().name()
 
             # Set label color to the dialog's chosen color
-            self.label_colorHex.setText(color)
+            self.setNewColor(color)
         else:
             print("Dialog cancelled")
 
-    def randomColor(self):
-        getRandomColor()
-        
-# API ACCESSORS
-def getRandomColor():
+    # Generates and returns a random color
+    def getRandomHexColor(self):
+        red   = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue  = random.randint(0, 255)
+        hex = '#%02x%02x%02x' % (red, blue, green) # CREDIT TO: Dietrich Epp --> https://stackoverflow.com/questions/3380726/converting-an-rgb-color-tuple-to-a-hexidecimal-string
+        return hex
 
-    query = "https://www.colourlovers.com/api/palettes/random?format=json"
-    response = requests.get(query) 
-    print(response.status_code)
+    # Generates and sets a new random color
+    def randomColor(self):
+        hex = self.getRandomHexColor()
+        self.setNewColor(hex)
+
+    # Changes the seed color title
+    # Changes the seed color image
+    # Changes the palette collection
+    def setNewColor(self, hex):
+        self.label_colorHex.setText(hex)
+
+    
 
 def main():
     app = QApplication([])
